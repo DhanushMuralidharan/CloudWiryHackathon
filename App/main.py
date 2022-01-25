59582f9d3ea0ec7a7b7ad2445ac56e1e01194041
DhanushMuralidharan <dhanushmuralidharan@gmail.com>
2022-01-25 21:26:24 +0530

    Add login page

--- App/main.py
+++ App/main.py
@@ -8,5 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return "Sign Up Page!"
+
 if __name__ == '__main__':
   app.run(debug=True,port=8000,host='127.0.0.1')
